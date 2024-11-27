b739a95a6023e432d2635267bea40f45957ec241
Rahul Pandey <104827030+rahulpandey535@users.noreply.github.com>
2024-11-27 11:07:08 +0530

    Add files via upload

---
+++ BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Rahul on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
---
+++ BarcodescannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodescannerTests.swift
+//  BarcodescannerTests
+//
+//  Created by Rahul on 08/11/24.
+//
+
+import Testing
+@testable import Barcodescanner
+
+struct BarcodescannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
